032ce19b9f7a55b56b40493f42247a3c5b5274d7
Jesus Castillo <jesusecb@gmail.com>
2021-02-19 19:56:16 -0300

    Desaroollo del frontend para buscar videos de youtube y mostrandolos, consumiento api en nodejs desplegada en heroku

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,60 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<v-app id=" app">
+
+		<v-app-bar
+			:clipped-left="$vuetify.breakpoint.lgAndUp"
+			app
+			color="blue darken-3"
+			dark
+		>
+			<img width="50" height="50" src="@/assets/youtube-icon.svg" />
+
+			<v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+				<span
+					style="font-family: 'Gill Sans Extrabold', Helvetica, sans-serif;"
+					class="hidden-sm-and-down"
+				>
+					Youtube Search
+				</span>
+			</v-toolbar-title>
+		</v-app-bar>
+		<v-main>
+			<v-slide-y-transition mode="out-in">
+				<router-view />
+			</v-slide-y-transition>
+		</v-main>
+
+		<v-footer 
+			dark 
+			padless 
+			app
+			:clipped-left="$vuetify.breakpoint.lgAndUp"
+		>
+			<v-card color="primary" class="flex" flat tile>
+				<v-card-text class="py-2 white--text text-center">
+					Jesus Castillo {{ new Date().getFullYear() }}
+				</v-card-text>
+			</v-card>
+		</v-footer>
+	</v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+	name: "App",
+	data() {
+		return {
+		};
+	},
+	computed: {
+	},
+	created() {
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+	},
+	methods: {
+	},
+};
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,92 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-container>
+		<v-row>
+			<v-col cols="12" sm="10" md="10">
+				<v-text-field
+					v-model="search"
+					label="Buscador"
+					placeholder="Escriba lo que desea buscar en youtube"
+					solo
+					@keyup.enter.native="buscar"
+				></v-text-field>
+				<span style="color:red" v-show="error">Tiene que escribir algun valor para buscar</span>
+			</v-col>
+			<v-col cols="12" sm="2" md="2">
+				<v-btn
+					color="primary"
+					style="height: 50px; min-width: 120px;"
+					@click="buscar"
+				>
+					Buscar
+				</v-btn>
+			</v-col>
+		</v-row>
+
+		<v-row dense>
+			<v-col v-for="(item, i) in items" :key="i" cols="12">
+				<v-card color="white">
+					<div class="d-flex flex-no-wrap ">
+						<v-img class="ma-3" max-width="300" tile>
+							<v-img :src="item.image"></v-img>
+						</v-img>
+						<div>
+							<v-card-title
+								class="headline"
+								v-text="item.title"
+							></v-card-title>
+
+							<v-card-subtitle
+								v-text="item.description"
+							></v-card-subtitle>
+							<v-card-actions>
+								<v-btn text :href= "'https://youtube.com/watch?v=' + item.videoId" target='_blank'>
+									ver video
+								</v-btn>
+            				</v-card-actions>
+						</div>
+
+					</div>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home",
+	data() {
+		return {
+			search: "",
+			items: [],
+			error: false
+		};
+	},
+	methods: {
+		async buscar() {
+			let me = this;
+			if (me.search.length == 0) {
+				me.error=true
+				
+			} else {
+				me.error=false
+				const data = {
+					search: me.search,
+				};
+				const res = await fetch(
+					"https://youtube-search-jc.herokuapp.com/api/youtube/search",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(data),
+					}
+				);
+				const results = await res.json();
+				me.items = results.data
+			}
+		},
+	},
+};
 </script>
